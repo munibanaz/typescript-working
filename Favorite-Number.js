6494b5127d4a5f9a12f6e90f2e321f1c35af9712
munibanaz <ahmedubaid36@Gmail.com>
2024-05-27 10:52:41 -0700

    Add files via upload

---
+++ Favorite-Number.js
@@ -0,0 +1,4 @@
+"use strict";
+const favorite_Number = 6;
+const massage = ' here is your favorite number ' + favorite_Number;
+console.log(massage);
---
+++ Favorite-Number.ts
@@ -0,0 +1,5 @@
+const favorite_Number : number = 6 ;
+
+const massage :string =' here is your favorite number ' + favorite_Number;
+
+console.log(massage);
